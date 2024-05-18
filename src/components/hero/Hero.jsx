c4764085b8a51ce669c0117a2245fbe3fe3f8d6a
LeshchuDev <leshchudev@gmail.com>
2024-05-18 15:27:59 +0200

    hero display flex

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -23,94 +23,83 @@ export default function Hero() {
   return (
     <div className="container-hero">
       <Navbar />
-      <motion.div
-        className="robot"
-        drag
-        variants={robotAnimation}
-        initial="height"
-        animate={{ y: [10, 0, 10], x: [10, 0, 10] }}
-        transition={{ ease: "easeInOut", duration: 4, repeat: Infinity }}
-      >
+      <div className="hero">
         <motion.div
-          className="radar"
-          animate={{ rotate: [0, 20, 0] }}
-          transition={{
-            animationTimingFunction: "linear",
-            repeat: Infinity,
-            duration: 4,
-          }}
-        ></motion.div>
-        <motion.div className="smile">
-          <motion.div className="smile1"></motion.div>
-          <motion.div className="smile2"></motion.div>
-          <motion.div className="smile3"></motion.div>
-          <motion.div className="smile4"></motion.div>
-          <motion.div className="smile5"></motion.div>
-          <motion.div className="smile6"></motion.div>
+          className="robot"
+          drag
+          variants={robotAnimation}
+          initial="height"
+          animate={{ y: [-10, -20, -10], x: [-10, 0, -10] }}
+          transition={{ ease: "easeInOut", duration: 4, repeat: Infinity }}
+        >
+          <motion.div
+            className="radar"
+            animate={{ rotate: [0, 20, 0] }}
+            transition={{
+              animationTimingFunction: "linear",
+              repeat: Infinity,
+              duration: 4,
+            }}
+          ></motion.div>
+          <motion.div className="smile">
+            <motion.div className="smile1"></motion.div>
+            <motion.div className="smile2"></motion.div>
+            <motion.div className="smile3"></motion.div>
+            <motion.div className="smile4"></motion.div>
+            <motion.div className="smile5"></motion.div>
+            <motion.div className="smile6"></motion.div>
+          </motion.div>
+          <motion.div className="eye1"></motion.div>
+          <motion.div className="eye2"></motion.div>
+          <motion.div
+            className="ear1"
+            variants={robotAnimation}
+            initial="height"
+            animate="low"
+            transition={{ ease: "easeInOut", duration: 4, repeat: Infinity }}
+          ></motion.div>
+          <motion.div
+            className="ear2"
+            variants={robotAnimation}
+            initial="height"
+            animate="low"
+            transition={{ ease: "easeInOut", duration: 4, repeat: Infinity }}
+          ></motion.div>
         </motion.div>
-        <motion.div className="eye1"></motion.div>
-        <motion.div className="eye2"></motion.div>
         <motion.div
-          className="ear1"
-          variants={robotAnimation}
-          initial="height"
-          animate="low"
-          transition={{ ease: "easeInOut", duration: 4, repeat: Infinity }}
-        ></motion.div>
-        <motion.div
-          className="ear2"
-          variants={robotAnimation}
-          initial="height"
-          animate="low"
-          transition={{ ease: "easeInOut", duration: 4, repeat: Infinity }}
-        ></motion.div>
-      </motion.div>
-      <motion.div
-        className="info"
-        initial="hidden"
-        animate="visible"
-        variants={textAnimation}
-      >
-        {" "}
-        {[...text1].map((letter, idx) => (
-          <motion.span
-            key={idx}
-            variants={textAnimation}
-            className="text1"
-          >
-            {letter}
-          </motion.span>
-        ))}
-        {[...text2].map((letter, idx) => (
-          <motion.span key={idx} variants={textAnimation} className="text2">
-            {letter}
-          </motion.span>
-        ))}
-        {[...text3].map((letter, idx) => (
-          <motion.span
-            key={idx}
-            variants={textAnimation}
-            className="text3"
-          >
-            {letter}
-          </motion.span>
-        ))}
-        {[...text4].map((letter, idx) => (
-          <motion.span key={idx} variants={textAnimation} className="text4">
-            {letter}
-          </motion.span>
-        ))}
-        {[...text5].map((letter, idx) => (
-          <motion.span
-            key={idx}
-            variants={textAnimation}
-            className="text5"
-          >
-            {letter}
-          </motion.span>
-        ))}
-      </motion.div>
-      <div className="next-info"></div>
+          className="info"
+          initial="hidden"
+          animate="visible"
+          variants={textAnimation}
+        >
+          {" "}
+          {[...text1].map((letter, idx) => (
+            <motion.span key={idx} variants={textAnimation} className="text1">
+              {letter}
+            </motion.span>
+          ))}
+          {[...text2].map((letter, idx) => (
+            <motion.span key={idx} variants={textAnimation} className="text2">
+              {letter}
+            </motion.span>
+          ))}
+          {[...text3].map((letter, idx) => (
+            <motion.span key={idx} variants={textAnimation} className="text3">
+              {letter}
+            </motion.span>
+          ))}
+          {[...text4].map((letter, idx) => (
+            <motion.span key={idx} variants={textAnimation} className="text4">
+              {letter}
+            </motion.span>
+          ))}
+          {[...text5].map((letter, idx) => (
+            <motion.span key={idx} variants={textAnimation} className="text5">
+              {letter}
+            </motion.span>
+          ))}
+        </motion.div>
+      </div>
     </div>
   );
 }
