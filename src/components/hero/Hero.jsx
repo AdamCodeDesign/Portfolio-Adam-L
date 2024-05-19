fa16b1a657e03c791b9f711dd37982b60293b609
LeshchuDev <leshchudev@gmail.com>
2024-05-19 11:46:11 +0200

    height of robot corected

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -24,48 +24,50 @@ export default function Hero() {
     <div className="container-hero">
       <Navbar />
       <div className="hero">
-        <motion.div
-          className="robot"
-          drag
-          variants={robotAnimation}
-          initial="height"
-          animate={{ y: [-10, -20, -10], x: [-10, 0, -10] }}
-          transition={{ ease: "easeInOut", duration: 4, repeat: Infinity }}
-        >
+        <div className="robot-container">
           <motion.div
-            className="radar"
-            animate={{ rotate: [0, 20, 0] }}
-            transition={{
-              animationTimingFunction: "linear",
-              repeat: Infinity,
-              duration: 4,
-            }}
-          ></motion.div>
-          <motion.div className="smile">
-            <motion.div className="smile1"></motion.div>
-            <motion.div className="smile2"></motion.div>
-            <motion.div className="smile3"></motion.div>
-            <motion.div className="smile4"></motion.div>
-            <motion.div className="smile5"></motion.div>
-            <motion.div className="smile6"></motion.div>
+            className="robot"
+            drag
+            variants={robotAnimation}
+            initial="height"
+            animate={{ y: [-10, -20, -10], x: [-10, 0, -10] }}
+            transition={{ ease: "easeInOut", duration: 4, repeat: Infinity }}
+          >
+            <motion.div
+              className="radar"
+              animate={{ rotate: [0, 20, 0] }}
+              transition={{
+                animationTimingFunction: "linear",
+                repeat: Infinity,
+                duration: 4,
+              }}
+            ></motion.div>
+            <motion.div className="smile">
+              <motion.div className="smile1"></motion.div>
+              <motion.div className="smile2"></motion.div>
+              <motion.div className="smile3"></motion.div>
+              <motion.div className="smile4"></motion.div>
+              <motion.div className="smile5"></motion.div>
+              <motion.div className="smile6"></motion.div>
+            </motion.div>
+            <motion.div className="eye1"></motion.div>
+            <motion.div className="eye2"></motion.div>
+            <motion.div
+              className="ear1"
+              variants={robotAnimation}
+              initial="height"
+              animate="low"
+              transition={{ ease: "easeInOut", duration: 4, repeat: Infinity }}
+            ></motion.div>
+            <motion.div
+              className="ear2"
+              variants={robotAnimation}
+              initial="height"
+              animate="low"
+              transition={{ ease: "easeInOut", duration: 4, repeat: Infinity }}
+            ></motion.div>
           </motion.div>
-          <motion.div className="eye1"></motion.div>
-          <motion.div className="eye2"></motion.div>
-          <motion.div
-            className="ear1"
-            variants={robotAnimation}
-            initial="height"
-            animate="low"
-            transition={{ ease: "easeInOut", duration: 4, repeat: Infinity }}
-          ></motion.div>
-          <motion.div
-            className="ear2"
-            variants={robotAnimation}
-            initial="height"
-            animate="low"
-            transition={{ ease: "easeInOut", duration: 4, repeat: Infinity }}
-          ></motion.div>
-        </motion.div>
+        </div>
         <motion.div
           className="info"
           initial="hidden"
