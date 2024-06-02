e95e2091c8d5689761464a3b65f1bf1d84123d2c
LeshchuDev <leshchudev@gmail.com>
2024-06-02 12:57:26 +0200

    overflow hidden on body also

--- src/components/bgdAnimations/BgdAnimations.jsx
+++ src/components/bgdAnimations/BgdAnimations.jsx
@@ -4,7 +4,7 @@ export default function BgdAnimations() {
   const skillName = ["react", "JS", "sass", "html", "RWD", "MUI", "git", "api"];
   
   return (
-    <div className="container-animations" >sticky
+    <div className="container-animations" >
       {skillName.map((el) => (
         <div className={el} key={el} >
           {el}
